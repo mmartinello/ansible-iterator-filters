b4b38d3a6b70f4c9b74c6b58d6aa4a7c6c43f2fb
Mattia Martinelllo <mattia@mattiamartinello.com>
2018-11-21 12:14:37 +0100

    Check for a single dictionary

--- iterator_filters.py
+++ iterator_filters.py
@@ -5,7 +5,9 @@ of object.
 Filters are available to check if the value is present in different type of
 given objects:
 - a single list
+- a single dictionary
 - a list containing multiple lists
+- a list containing multiple dictionaries
 - a dictionary containing multiple lists
 - a dictionary containing multiple dictionaries
 """
@@ -20,4 +22,8 @@ class FilterModule(object):
     def in_list(self, needle, haystack):
         return needle in haystack
 
-#    def in_dict(self, needle, haystack):
+    def in_dict(self, needle, haystack):
+        for key, value in haystack.items():
+            if needle == value:
+                return True
+        return False
--- test.py
+++ test.py
@@ -1,14 +1,37 @@
 import unittest
 from iterator_filters import FilterModule
- 
+
+
 class IteratorFiltersTest(unittest.TestCase):
- 
+
     def setUp(self):
         self.filter = FilterModule()
 
     def test_string_in_list(self):
-        a = ["foo", "bar"]
-        self.assertTrue(self.filter.in_list("foo", a))
- 
+        list = ["foo", "bar"]
+        string = "foo"
+        self.assertTrue(self.filter.in_list(string, list))
+
+    def test_string_not_in_list(self):
+        list = ["foo", "bar"]
+        string = "baz"
+        self.assertFalse(self.filter.in_list(string, list))
+
+    def test_string_in_dict(self):
+        dict = {
+            'key1': "foo",
+            'key2': "bar"
+        }
+        string = "foo"
+        self.assertTrue(self.filter.in_dict(string, dict))
+
+    def test_string_not_in_dict(self):
+        dict = {
+            'key1': "foo",
+            'key2': "bar"
+        }
+        string = "baz"
+        self.assertFalse(self.filter.in_dict(string, dict))
+
 if __name__ == '__main__':
     unittest.main()
