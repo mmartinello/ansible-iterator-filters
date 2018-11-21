aa2a6508b46cdb78ca940cbe60b38fe23bb20fec
Mattia Martinelllo <mattia@mattiamartinello.com>
2018-11-21 12:24:56 +0100

    Docstrings

--- iterator_filters.py
+++ iterator_filters.py
@@ -15,15 +15,49 @@ given objects:
 
 class FilterModule(object):
     def filters(self):
+        """Dictionary with the list of filters which can be used from Ansible.
+
+        The dictionary must contain a list of filters in the form of:
+        'filter_name': function to execute
+
+        Example:
+            Declare a filter named "foo_filter" which executed the
+            self.bar_function function:
+
+                return {
+                    'foo_filter': self.bar_function,
+                }
+        """
         return {
             'in_list': self.in_list,
         }
 
-    def in_list(self, needle, haystack):
-        return needle in haystack
+    def in_list(self, string, list):
+        """Checks if a given string is into one of the value of the given list.
 
-    def in_dict(self, needle, haystack):
-        for key, value in haystack.items():
-            if needle == value:
+        Args:
+            string(str): the string to look for.
+            list(list): the list in which the string should be searched.
+
+        Returns:
+            True if the string is included in at least one of the list's
+                values, False otherwise.
+        """
+        return string in list
+
+    def in_dict(self, string, dictionary):
+        """Checks if a given string is into one of the value of the given list.
+
+        Args:
+            string(str): the string to look for.
+            dictionary(dict): the dictionary in which the string should be
+                searched.
+
+        Returns:
+            True if the string is included in at least one of the dictionary's
+                values, False otherwise.
+        """
+        for key, value in dictionary.items():
+            if string == value:
                 return True
         return False
