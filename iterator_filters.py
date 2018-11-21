2da93acb126af3c78458287c2b02bdfcb6f0d657
Mattia Martinelllo <mattia@mattiamartinello.com>
2018-11-21 11:40:35 +0100

    Return in one line

--- iterator_filters.py
+++ iterator_filters.py
@@ -1,4 +1,13 @@
-# This filter plugin will search a given value into every subkey of a given list
+"""iterator_filters.py
+Ansible custom filter to check if a given value is present into a given list of object.
+
+Filters are available to check if the value is present in different type of given objects:
+- a single list
+- a list containing multiple lists
+- a dictionary containing multiple lists
+- a dictionary containing multiple dictionaries
+"""
+
 class FilterModule(object):
     def filters(self):
         return {
@@ -6,7 +15,7 @@ class FilterModule(object):
         }
 
     def in_list(self, needle, haystack):
-        if needle in haystack:
-            return True
-        else:
-            return False
+        return needle in haystack
+
+#    def in_dict(self, needle, haystack):
+    
